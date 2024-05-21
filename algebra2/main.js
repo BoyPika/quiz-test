be6c2ccd55feeea43385d9af73567cce9beade9c
BoyPika <boypika85@gmail.com>
2024-05-21 00:16:50 -0400

    Add Algebra 2 practice test files

---
+++ algebra2/main.js
@@ -0,0 +1,8 @@
+import "../src/app.css";
+import Algebra2 from "./Algebra2.svelte";
+
+const app = new Algebra2({
+  target: document.getElementById("app"),
+});
+
+export default app;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import mkcert from'vite-plugin-mkcert'
+import { resolve } from 'path'
+
+const outDir = resolve(__dirname, 'dist')
+const html = 'index.html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, html),
+            algebra2: resolve(__dirname, 'algebra2/index.html')
+        },
+    },
+},
   plugins: [svelte(), mkcert()],
 })
