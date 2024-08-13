4704e9bec847bb6e67671346fed3e29050f069cc
BoyPika <boypika85@gmail.com>
2024-08-13 14:02:26 -0400

    add results page for chemistry and algebra 2 with a download link for the pdf generated by the golang backend

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,6 @@ export default defineConfig({
             main: resolve(__dirname, html),
             algebra2: resolve(__dirname, 'algebra2/index.html'),
             chemistry: resolve(__dirname, 'chemistry/index.html'),
-            chemistryresults: resolve(__dirname, 'chemistry/results/index.html'),
         },
     },
 },
